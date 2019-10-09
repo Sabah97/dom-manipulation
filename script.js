203285e984bd21b70a5f130d927d4321579a22f0
Sabah97 <sabah.ashraf97@gmail.com>
2019-10-09 08:49:41 +0600

    Task2

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 var heading=document.getElementById('heading')
 console.log(heading.textContent);
 /* Task 2 using plain JavaScript */
-
+function addText(event){
+var p= document.createElement('p')
+p.innerText="Hello World"
+var div=document.getElementById('task2a')
+div.appendChild(p)
+}
 
 
 /* Task 4 using jQuery */
